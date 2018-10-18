4d34b3a08512f856a74a3f89129f9f8281686462
art <swsms@mail.ru>
2018-10-18 21:49:01 +0500

    Create the basic project's structure

---
+++ client/src/main/java/org/artb/onlinechat/clientside/Client.java
@@ -0,0 +1,8 @@
+package org.artb.onlinechat.clientside;
+
+public class Client {
+
+    public static void main(String[] args) {
+        System.out.println("Hello from chat-client!");
+    }
+}
---
+++ server/src/main/java/org/artb/onlinechat/serverside/Server.java
@@ -0,0 +1,8 @@
+package org.artb.onlinechat.serverside;
+
+public class Server {
+
+    public static void main(String[] args) {
+        System.out.println("Hello from chat-server!");
+    }
+}
