61150e04af492c6a4bb18ece3629fdf6f0bfb5dd
Artyom Burylov <swsms@mail.ru>
2018-11-10 09:50:58 +0500

    Add comment

--- server/src/main/kotlin/org/artb/onlinechat/serverside/command/ListUsersCommand.kt
+++ server/src/main/kotlin/org/artb/onlinechat/serverside/command/ListUsersCommand.kt
@@ -5,7 +5,7 @@ import org.artb.onlinechat.serverside.ClientHandler
 class ListUsersCommand: Command {
 
     override fun execute(handler: ClientHandler) {
-        val users = listOf("Homer J. Simpson", "John Smith", "Alice Bright")
+        val users = listOf("Homer J. Simpson", "John Smith", "Alice Bright") // TODO load the list from server
         handler.sendMsg(users.toString())
     }
 }
